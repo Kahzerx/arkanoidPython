220dfeefb7d56b23648ba35b1b6aca1ee30ccdbc
kahzerx <kahzer123@gmail.com>
2019-12-07 16:52:43 +0100

    rediseño de la parte princpal, ahora la bola es capaz de reconocer cada ladrillo por individual

--- venv/lib/arkanoid.py
+++ venv/lib/arkanoid.py
@@ -31,15 +31,15 @@ class Bola(pygame.sprite.Sprite):
         self.rect.left = x
         self.rect.top = y
         self.image.fill(White)
-        self.movimiento = [3, -3]  # movimiento bola (x, y)
+        self.movimiento = [4, -4]  # movimiento bola (x, y)
         self.name = 'ball'
 
     def crea(self):
         pantalla.blit(self.image, self.rect)
 
-    def actualiza(self, cursor, ladrillos):
+    def actualiza(self, cursor, blocks):
         self.rect = self.rect.move(self.movimiento)
-        self.detecta(cursor, ladrillos)
+        self.detecta(cursor, blocks)
         self.limites()
 
     def limites(self):
@@ -49,12 +49,11 @@ class Bola(pygame.sprite.Sprite):
         if self.rect.top < 0:  # techo
             self.movimiento[1] *= -1
 
-    def detecta(self, cursor, ladrillos):
-        golpe = pygame.sprite.Group(cursor, ladrillos)
+    def detecta(self, cursor, blocks):
+        golpe = pygame.sprite.Group(cursor, blocks)
         lista = pygame.sprite.spritecollide(self, golpe, False)
         if len(lista) > 0:
             for sprite in lista:
-                print(sprite)
                 posicion = self.rect[0] - sprite.rect[0]
                 """ni idea de como conseguir donde colisiona asi que calcula la x de la posicion del cursor 
                 y la x de la posicion de la bola y las resto"""
@@ -69,6 +68,8 @@ class Bola(pygame.sprite.Sprite):
                         self.movimiento[0] = 3
                     else:
                         self.movimiento[0] = 4.5
+                elif sprite.name == 'bloque':
+                    sprite.kill()
 
             self.movimiento[1] *= -1
 
@@ -101,25 +102,12 @@ class Cursor(pygame.sprite.Sprite):
 
 
 class Ladrillos(pygame.sprite.Sprite):
-    def __init__(self, ancho, alto):
+    def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((ancho, alto))
+        self.image = pygame.Surface((60, 20))
         self.image.fill(White)
         self.rect = self.image.get_rect()
         self.name = 'bloque'
-
-    def creaBloques(self):
-        blocks = pygame.sprite.Group
-        for fila in range(10):
-            for columna in range(9):
-                self.rect.x = 1 + columna * (60 + 7)  # ancho ladrillo + espacio
-                self.rect.y = 1 + fila * (20 + 7)  # alto ladrillo + espacio
-                if columna == 0 or columna % 2 == 0:
-                    self.image.fill(Grey)
-                else:
-                    self.image.fill(Red)
-
-                pantalla.blit(self.image, self.rect)
 
 
 class Juego:
@@ -127,6 +115,11 @@ class Juego:
         pygame.init()
         self.pantalla, self.rect = self.screen()
         self.mousex = 0
+        self.blocks = self.creaBloques()
+        self.bola = self.creaBola()
+        self.cursor = self.creaCursor()
+
+        self.todosLosSprites = pygame.sprite.Group(self.blocks, self.bola, self.cursor)
 
     def screen(self):
         pygame.display.set_caption('Arkanoid')
@@ -135,11 +128,31 @@ class Juego:
 
         return pantalla, rect
 
+    def creaBola(self):
+        bola = Bola(500, 500, 10, 10)  # constructor de la bola
+        return bola
+
+    def creaCursor(self):
+        cursor = Cursor(ANCHO_PANTALLA / 2, ALTO_PANTALLA - ALTO_PANTALLA / 10, 80, 10)  # constructor del cursor
+        return cursor
+
+    def creaBloques(self):
+        blocks = pygame.sprite.Group()
+
+        for fila in range(10):
+            for columna in range(9):
+                block = Ladrillos()
+                block.rect.x = 1 + columna * (60 + 7)  # ancho ladrillo + espacio
+                block.rect.y = 1 + fila * (20 + 7)  # alto ladrillo + espacio
+                if columna == 0 or columna % 2 == 0:
+                    block.image.fill(Grey)
+                else:
+                    block.image.fill(Red)
+                blocks.add(block)
+        return blocks
+
     def bucle(self):
         game_over = False
-        cursor = Cursor(ANCHO_PANTALLA / 2, ALTO_PANTALLA - ALTO_PANTALLA / 10, 80, 10)  # constructor del cursor
-        bola = Bola(500, 500, 10, 10)  # constructor de la bola
-        ladrillos = Ladrillos(60, 20)  # constructor de ladrillos
         while not game_over:
             for event in pygame.event.get():  # detecta clicks y teclas
                 if event.type == pygame.QUIT:  # detecta solo cuando haces click en cerrar la ventana
@@ -150,13 +163,12 @@ class Juego:
 
             pantalla.fill(Black)
 
-            ladrillos.creaBloques()
+            self.todosLosSprites.update(self.mousex, self.blocks, self.cursor)
+            self.todosLosSprites.draw(self.pantalla)
 
-            bola.actualiza(cursor, ladrillos)
-            bola.crea()
+            self.cursor.actualiza(self.mousex)
 
-            cursor.actualiza(self.mousex)
-            cursor.crea()
+            self.bola.actualiza(self.cursor, self.blocks)
 
             pygame.display.update()
 
